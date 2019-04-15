6799efac7fb95f3bd3f96bc989bb74231535dd37
Gabe Payne <gabrielmpayne@gmail.com>
2019-04-15 16:13:51 -0500

    finally got grid to appear

--- etchasketch/app.js
+++ etchasketch/app.js
@@ -1,17 +1,26 @@
 const grid = document.getElementById("game-board");
+const square = document.getElementsByClassName("square");
 
 function main() {
 	for(var i=0; i < 8; i++){
 		for(var j=0; j < 8; j++){
 		var newDiv = document.createElement('div');
-	   	newDiv.id = 'box'+i;
+		var br = document.createElement('br');
 	  	newDiv.className = 'square';
-	  	grid.appendChild(newDiv);
-	  }
-	  	grid.appendChild(newDiv);
+	 	grid.appendChild(newDiv);
+	  	}
+	  grid.appendChild(newDiv);
+	  grid.appendChild(br);
 	}
-
-	alert(newDiv.id);
 }
+
+square.onclick = function(){
+	if(document.square.style.backgroundColor =="white"){
+		document.square.style.backgroundColor="black";
+	}
+	if(document.square.style.backgroundColor =="black") {
+		document.square.style.backgroundColor = "white";
+	}
+};
 
 main();
