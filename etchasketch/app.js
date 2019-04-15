87fb25dcf40ef3eef8f77f10104efa4658363241
Gabe Payne <gabrielmpayne@gmail.com>
2019-04-15 01:07:33 -0500

    broke some stuff for sure

--- etchasketch/app.js
+++ etchasketch/app.js
@@ -1,9 +1,17 @@
-var toAdd = document.createDocumentFragment();
-for(var i=0; i < 11; i++){
-   var newDiv = document.createElement('div');
-   newDiv.id = 'r'+i;
-   newDiv.className = 'ansbox';
-   toAdd.appendChild(newDiv);
+const grid = document.getElementById("game-board");
+
+function main() {
+	for(var i=0; i < 8; i++){
+		for(var j=0; j < 8; j++){
+		var newDiv = document.createElement('div');
+	   	newDiv.id = 'box'+i;
+	  	newDiv.className = 'square';
+	  	grid.appendChild(newDiv);
+	  }
+	  	grid.appendChild(newDiv);
+	}
+
+	alert(newDiv.id);
 }
 
-document.appendChild(toAdd);
+main();
