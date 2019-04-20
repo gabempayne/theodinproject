947831bb4881efdf36af80b89742109bb70fa0ca
Gabe Payne <gabrielmpayne@gmail.com>
2019-04-20 04:04:13 -0500

    more styling for this awesome calculator

--- calculator/app.js
+++ calculator/app.js
@@ -1,3 +1,25 @@
+const outputBox = document.getElementById('output-box');
+const sevenBtn = document.getElementById('seven');
+const eightBtn = document.getElementById('eight');
+const nineBtn = document.getElementById('output-box');
+const fourBtn = document.getElementById('output-box');
+const sixBtn = document.getElementById('output-box');
+const oneBtn = document.getElementById('output-box');
+const twoBtn = document.getElementById('output-box');
+const threeBtn = document.getElementById('output-box');
+const clearBtn = document.getElementById('output-box');
+const zeroBtn = document.getElementById('output-box');
+const equalsBtn = document.getElementById('output-box');
+const divideBtn = document.getElementById('output-box');
+const timesBtn = document.getElementById('output-box');
+const minusBtn = document.getElementById('output-box');
+const plusBtn = document.getElementById('output-box');
+
+
+
+
+
+
 // Add function
 function add(a, b) {
 	return a + b;
@@ -16,5 +38,5 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-	
+
 }
