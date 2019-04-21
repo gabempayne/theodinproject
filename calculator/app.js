95e4eb978eb3a1e5c53b6808970cb06ee66e4250
Gabe Payne <gabrielmpayne@gmail.com>
2019-04-21 05:55:53 -0500

    i have no idea what I'm doing

--- calculator/app.js
+++ calculator/app.js
@@ -1,23 +1,19 @@
 const outputBox = document.getElementById('output-box');
 const sevenBtn = document.getElementById('seven');
 const eightBtn = document.getElementById('eight');
-const nineBtn = document.getElementById('output-box');
-const fourBtn = document.getElementById('output-box');
-const sixBtn = document.getElementById('output-box');
-const oneBtn = document.getElementById('output-box');
-const twoBtn = document.getElementById('output-box');
-const threeBtn = document.getElementById('output-box');
-const clearBtn = document.getElementById('output-box');
-const zeroBtn = document.getElementById('output-box');
-const equalsBtn = document.getElementById('output-box');
-const divideBtn = document.getElementById('output-box');
-const timesBtn = document.getElementById('output-box');
-const minusBtn = document.getElementById('output-box');
-const plusBtn = document.getElementById('output-box');
-
-
-
-
+const nineBtn = document.getElementById('nine');
+const fourBtn = document.getElementById('four');
+const sixBtn = document.getElementById('five');
+const oneBtn = document.getElementById('six');
+const twoBtn = document.getElementById('one');
+const threeBtn = document.getElementById('two');
+const clearBtn = document.getElementById('three');
+const zeroBtn = document.getElementById('clear');
+const equalsBtn = document.getElementById('zero');
+const divideBtn = document.getElementById('equals');
+const timesBtn = document.getElementById('times');
+const minusBtn = document.getElementById('minus');
+const plusBtn = document.getElementById('plus');
 
 
 // Add function
@@ -28,11 +24,11 @@ function add(a, b) {
 function sub(a, b) {
 	return a - b;
 }
-// sub multiply
+// Multi function
 function multi(a, b) {
 	return a * b;
 }
-
+// Division function
 function divide(a, b) {
 	return a / b;
 }
@@ -40,3 +36,11 @@ function divide(a, b) {
 function operate(operator, a, b) {
 
 }
+
+sevenBtn.click(function() {
+	outputBox.value = 'help';
+});
+
+
+
+// main();
