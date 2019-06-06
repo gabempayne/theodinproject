ce753bb89693f39638b5108ce44bb93e86afb368
Gabe Payne <gabrielmpayne@gmail.com>
2019-06-05 23:02:18 -0500

    check icon class toggle

--- javascript/library/script.js
+++ javascript/library/script.js
@@ -2,7 +2,7 @@
 /// will be changing to a list set up instead of an image setup
 
 const delBtn = document.getElementById('deletebtn');
-const checkMark = document.getElementById('checkMark');
+const checkMark = document.getElementById('checkmark');
 
 let myLibrary = [];
 
@@ -16,6 +16,17 @@ function Book(author, title, pages, has_read) {
 function main() {
 	var book = new Book('JRR Tolkien', 'The Two Towers', '430', true);
 	addBooktoLibrary(book);
+
+		checkMark.addEventListener('click', function(){
+		if(checkMark.classList.contains('checkmarkNotRead')) {
+			checkMark.classList.add('checkmarkRead');
+			checkMark.classList.remove('checkmarkNotRead');
+		}
+		if(checkMark.className.classList.contains('checkmarkRead')) {
+			checkMark.classList.add('checkmarkNotRead');
+			checkMark.classList.remove('checkmarkRead');
+		}
+	});
 }
 
 function addBooktoLibrary(book) {
@@ -33,23 +44,13 @@ function removeBookfromLibrary() {
 }
 
 function addBookButton() {
-	checkMark.addEventListener('click', function() {
-		if(checkMark.classList.contains('checkmarkNotRead')) {
-			checkMark.classList.add('checkmarkRead');
-			checkMark.classList.remove('checkmarkNotRead');
-		}
-		if(checkMark.className.classList.contains('checkmarkRead')) {
-			checkMark.classList.add('checkmarkNotRead');
-			checkMark.classList.remove('checkmarkRead');
-		}
-	});
+	
 }
 
 function hasRead() {
 
 }
 
-
-
-
-main();
+window.onload=function(){
+  main();
+}
